60c3b3983b0b380b98d4c9c470c579a3a18de0bc
Mayur B Patel <mayur.b.patel@1rivet.com>
2024-07-22 18:48:28 +0530

    Add : initial commit

---
+++ src/routes/index.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from "express";
+
+const router = express.Router();
+
+router.post(
+  "/register",
+  (req: Request, res: Response, next: Function): void => {
+    if (!req.body.firstName) {
+      res.status(400).json({ message: "First name is required" });
+    }
+    res.status(201).json({ message: "User created" });
+  }
+);
+export default router;
